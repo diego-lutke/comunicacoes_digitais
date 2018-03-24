198798c5e58d300ef12b7623ed13efe46c0960ab
Diego Lütke <diegolutke@hotmail.com>
2018-03-24 15:32:35 -0300

    Primeira conclusão do PSK de qualquer ordem; Início da análise de erros utilizando o QPSK.

--- 1 - BPSK.py
+++ 1 - BPSK.py
@@ -1,16 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import matplotlib.pyplot as plt
 import numpy as np
+import math as mt
 
-Fs = 150.0
+'''Fs = 150.0
 Ts = 1.0/Fs
 t = np.arange(0,1.,Ts)
-d = [1., 1., 1., 0., 1., 1.]
+
+r = raw_input("Você deseja digitar os dados que serão transmitidos? (s/n)\n")
+if r == 's':
+    d = raw_input("Digite os dados que você deseja:\n Ex. 00 00 01 11 00 01 11 11\n")
+    d = d.replace(" ", "")
+    dlist = []
+    n = 0
+    while (n <> len(d)):
+        dlist.append(d[n:n + 2])
+        n = n + 2
+elif r == 'n':
+    k = raw_input("Criarei os dados para você, mas para isso, me diga a quantidade de bits que você deseja enviar:\n")
+    conj = int(k)
+    dlist = []
+    aur = []
+    while conj <> 0:
+        aur = np.random.randint(2)
+        dlist.append(aur)
+        conj = conj - 1
+    print(dlist)
+
 ff = 5
 s = []
 z = []
-tn = np.arange(0,len(d)*1.,Ts)
+tn = np.arange(0,len(dlist)*1.,Ts)
 
-for x in d:
+for x in dlist:
     if x==1:
         for y in t:
             z.append(1)
@@ -20,13 +43,39 @@ for x in d:
         for y in t:
             z.append(0)
             k = np.sin(2 * np.pi * ff * y + np.pi)
-            s.append(k)
+            s.append(k)'''
 
-fig, ax = plt.subplots(2, 1)
+EboverNo = [0., 1., 2., 3., 4., 5., 6., 7., 8., 8., 10.]
+ber = []
+
+for b in EboverNo:
+    lin = 10.0**(b/10.0)
+    x = np.random.randint(2,size=5000000)
+    bpsk = 2*x - 1
+    std = 1.0/np.sqrt(2*lin)
+    y = bpsk + std*np.random.randn(len(x))
+    rx = (y >= 0)
+    erro = (rx != x).sum()
+    ber.append(float(erro)/float(len(x)))
+
+'''x = n.pr
+x_red npconcatenate([x,y],axis=)
+
+y1 = ylab'b]
+y2 = y[b*1.fim]'''
+
+print(ber)
+
+'''fig1, ax = plt.subplots(2, 1)
 ax[0].plot(tn,z)
 ax[0].set_xlabel('Bit')
 ax[0].set_ylabel('Tempo')
 ax[1].plot(tn,s)
 ax[1].set_xlabel('Freq')
 ax[1].set_ylabel('Tempo')
+plt.show()'''
+
+plt.plot(EboverNo,ber)
+plt.yscale('log')
+plt.xscale('linear')
 plt.show()
--- 3 - PSK-N_Order.py
+++ 3 - PSK-N_Order.py
@@ -2,11 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-import matplotlib.pyplot as plt
 import random as rd
 import itertools as it
+import matplotlib.pyplot as plt
+import math as mt
+from bitstring import BitArray
 
-n = raw_input("Quantos símbolos você deseja?\n")
+n = raw_input("Em qual ordem você deseja seu PSK? Lembre-se que a ordem de um PSK de qualquer ordem é igual a 2^n.\n")
+n = int(mt.log(float(n),2))
 check2 = False
 while (check2 <> True):
     r = raw_input("Você deseja digitá-los? (s/n)\n")
@@ -16,7 +19,7 @@ while (check2 <> True):
             d = raw_input("Digite os dados que você deseja:\n Ex. 00 00 01 11 00 01 11 11\n")
             d = d.replace(" ", "")
 
-            if len(d) % int(n) == 0:
+            if len(d) % n == 0:
                 check1 = True
             else:
                 print('Faltou um dígito em sua entrada. Faça novamente!')
@@ -25,13 +28,14 @@ while (check2 <> True):
         dlist = []
         w = 0
         while (w <> len(d)):
-            dlist.append(d[w:w + int(n)])
-            w = w + int(n)
+            dlist.append(d[w:w + n])
+            w = w + n
         print(dlist)
         check2 = True
+        conj = len(dlist)
     elif r == 'n':
         k = raw_input("Criarei os dados para você, mas para isso, me diga a quantidade de conjuntos de dados que serão enviados:\n")
-        conj = int(k)*int(n)
+        conj = int(k)*n
         dlist = []
         while conj <> 0:
             aur = rd.randrange(2)
@@ -49,10 +53,52 @@ ff = 5
 
 s = []
 z = []
-tn = np.arange(0,len(dlist)*1.,Ts)
+tn = np.arange(0,len(dlist)/n,Ts)
+dlist = [dlist[x:x+n] for x in xrange(0, len(dlist), n)]
 
-compar = list(it.product([0, 1], repeat=int(n)))
+nfases = 2**n
+fases = (2.*np.pi)/nfases
 
-for x in dlist:
-    for y in compar:
+fasesl = []
+k = 0
+while k < nfases:
+    fasesl.append(fases*k)
+    k = k + 1
+
+compar = list(it.product([0, 1], repeat=n))
+comparjoin = list(it.chain.from_iterable(compar))
+
+mix = []
+k = n
+for pp in fasesl:
+    comparjoin.insert(k,pp)
+    k = k + n + 1
+
+comparjoin = np.asarray(comparjoin,dtype=np.float64)
+comparjoin = comparjoin.reshape(nfases,(n+1))
+
+dlist = np.asarray(dlist,dtype=np.float64)
+
+print(comparjoin)
+print(dlist)
+
+for pp in dlist:
+    for kk in comparjoin:
+        juncao1 = "".join(str(pp[0:n]))
+        juncao2 = "".join(str(kk[0:n]))
+        if juncao1 == juncao2:
+            for y in t:
+                c = BitArray(pp)
+                z.append(c.uint)
+                k = np.cos(2 * np.pi * ff * y + kk[n])
+                s.append(k)
+
+fig, ax = plt.subplots(2, 1)
+ax[0].plot(tn,z)
+ax[0].set_xlabel('Tempo')
+ax[0].set_ylabel('Bit')
+ax[1].plot(tn,s,'r')
+ax[1].set_xlabel('Tempo')
+ax[1].set_ylabel('Amplitude')
+plt.show()
 
